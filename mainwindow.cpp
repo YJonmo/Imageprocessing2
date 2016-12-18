dfc759b608cadf86fefc103c589702c51561b0ea
YJonmo <y.jonmo@auckland.ac.nz>
2016-12-19 08:25:55 +1300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,7 +9,10 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    //ROII = 10;
+    // Initializing
+    Instance = 0;
+    ROII = 200;
+    ThreshVale = 0;
     ui->setupUi(this);
 }
 
@@ -21,12 +24,27 @@ MainWindow::~MainWindow()
 void MainWindow::on_ROI_clicked()
 {
     QString ROISize = ui->InputROI->text();
-    ui->Replica->setText((ui->InputROI->text()));
+    //ui->Replica->setText((ui->InputROI->text()));
     std::cout << ROISize.toStdString() << std::endl;
     //return (ROISize.toStdString());
     ROII = ROISize.split(" ")[0].toInt();
     //ROII = ROISize.toStdString());
+    QString ThreshValueS = ui->Replica->text();
+    ThreshVale = ThreshValueS.split(" ")[0].toInt();
 }
 
 
+void MainWindow::on_radioButton_clicked()
+{
+    Instance = 1;
+}
 
+void MainWindow::on_radioButton_2_clicked()
+{
+    Instance = 2;
+}
+
+void MainWindow::on_radioButton_3_clicked()
+{
+    Instance = 3;
+}
